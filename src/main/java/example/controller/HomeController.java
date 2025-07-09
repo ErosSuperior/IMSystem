0f159a6cda34a51df18fea0edd6c8f1859b7ef47
ErosSuperior <lucnbhe187315@fpt.edu.vn>
2025-07-09 14:16:43 +0700

    Homepage

--- src/main/java/example/controller/HomeController.java
+++ src/main/java/example/controller/HomeController.java
@@ -1,12 +1,15 @@
 package example.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
     @GetMapping("/home")
-    public String home() {
-        return "Home"; // Trả về file home.html trong thư mục templates
+    public String home(HttpServletRequest request, Model model) {
+        model.addAttribute("currentPath", request.getRequestURI());
+        return "Home";
     }
 }
